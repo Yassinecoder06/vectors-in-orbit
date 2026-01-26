c287ace37145f5af569c4f8ded48cbc1e19a5ae5
Yassinecoder06 <yassinejemmali45@gmail.com>
2026-01-26 22:03:44 +0100

    Remove tracked directories, now in gitignore

--- cf/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Financial-Aware Collaborative Filtering utilities."""
-
-from .fa_cf import (
-    compute_financial_alignment,
-    get_fa_cf_scores,
-    build_user_interaction_profile,
-)
--- explanations/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Explanation generation helpers for recommendations."""
-
-from .generator import build_explanations
